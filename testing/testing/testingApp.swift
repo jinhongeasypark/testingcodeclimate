76175c6214c010a1a7bb22d22623f8aa718ef6b8
jin <jin.hong@easypark.net>
2022-04-28 14:47:39 +0200

    initial commit

---
+++ testing/testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by jk on 2022-04-28.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
